026f6f1cd70079a44019472f9159eea1109aaed1
Chuong Ngo <cngo.github@gmail.com>
2018-05-01 11:54:01 -0400

    Updated tests.

--- pytest_tornadis/tests/test_mockclient.py
+++ pytest_tornadis/tests/test_mockclient.py
@@ -69,7 +69,7 @@ async def test_mockclient_get(mock_client):
 async def test_mockclient_set(mock_client):
     assert 'test' not in mock_client.data
     result = await mock_client.call('SET', 'test', 'foo')
-    assert mock_client.data['test'] == 'foo'
+    assert mock_client.data['test'] == (clients.RedisCommands.SET, 'foo')
 
 @pytest.mark.gen_test
 @pytest.mark.usefixtures('mock_client')
